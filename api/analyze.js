c2460dbb25ee0c4eeac951dd1d64ebaaa8d98bd6
ProgrammerSahil <74780160+ProgrammerSahil@users.noreply.github.com>
2024-01-19 18:43:15 +0530

    Add files via upload

--- api/analyze.js
+++ api/analyze.js
@@ -1,22 +1,21 @@
 const { Configuration, OpenAI } = require("openai");
 
-const openai = new OpenAI({
-  apiKey: 'enterYourAPIkey'
-});
-
-const analyzeContract = async (contractText) => {
+const analyzeContract = async (contractText, API_KEY) => {
   try {
+    console.log(API_KEY);
+    const openai = new OpenAI({
+      apiKey: API_KEY ,
+    });
     const prompt = `Give the explanation in points and add a @ before every point Analyze the following contract and summarize its main points, highlight anything that could potentially help the user and anything that could be dangerous to the user, also provide suggestions to improve the contract :\n\n${contractText}\n `;
-    
+
     const chatCompletion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
-      messages: [{"role": "user", "content": prompt}],
+      messages: [{ role: "user", content: prompt }],
     });
     return chatCompletion.choices[0].message.content;
   } catch (error) {
-    console.error('Error in analyzing contract:', error);
+    console.error("Error in analyzing contract:", error);
     throw error;
   }
 };
-
 module.exports = analyzeContract;
